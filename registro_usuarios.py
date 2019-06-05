42bb564e96b01cbe8eb9c1e72eeac82436465d9e
frang <frangcong@gmail.com>
2019-06-05 20:05:38 -0300

    se hace modificaciones sin resultante optimo. Se requiere reajustar el codigo para incluir un menu; ademas de establecer la consulta de instructivo sobre el juego.

--- registro_usuarios.py
+++ registro_usuarios.py
@@ -1,4 +1,44 @@
+#import sqlite3
+
 from tabulate import tabulate
+#import turtle
+window = Tk()
+menu = Menu(window)
+window.config(menu = menu)
+filemenu = Menu(menu)
+menu.add_cascade(label = "juego nuevo", menu = filemenu)
+#se crea la funcion de culsuntar o no por las instrucciones del juego.
+
+def consulta_instrucciones():
+    I_consulta = input('deseas consultar las instrucciones acerca del juego de "La Generala" => S / N : ').upper()
+    if (I_consulta == "S"):
+
+#def procesa_turno_jugador(jugador_nombre):
+#El código ejecuta el turno para cada jugador.
+#Si devuelve False, continuamos con el juego.
+#Si devuelve True, alguien ha ganado así que paramos. """
+    #psi, angulo = entrada_usuario = obtener_datosdel_usuario(jugador_nombre)
+
+    #distancia_boladebarro = calcular_distancia(psi, angulo)
+    #diferencia = distancia_boladebarro - distancia_aparte
+
+# Si echamos un vistazo al capítulo de formatos de impresión, estas líneas
+# podrían imprimir números en un bonito formato.
+
+    #if diferencia > 1:
+        #print("Ha caído", diferencia, "metros muy lejos!")
+    #elif diferencia < -1:
+        #print("Te has quedado", diferencia * -1, "metros corto!")
+    #else:
+        #print("Bingo!", jugador_nombre, "gana!")
+        #return True
+
+    #print()
+    #return False
+
+#se hace el registro de cant y nombre de jugadores y los mismos se muestran en una tabla.
+
+
 
 nombres = []
 puntos = []
